786deae5cdcdd3a578cb9e02a787e3854972bec9
rathnatestingRathna-omkar <learning.loa@gmail.com>
2017-08-15 13:34:01 +0530

    Java Collection Notes

---
+++ src/test/java/collections/Notes.java
@@ -0,0 +1,31 @@
+package collections;
+
+public class Notes {
+	/*
+Java Collection framework is a set of classes and interfaces that implement commonly reusable collection data structures.
+Collection framework allows us to manipulate and store group of objects. Because in collection framework we cannot use primitive data types, we can use only objects.
+Collection interfaces and classes are present in java.util package
+
+ArrayList:
+ArrayList is used to store group of objects/data.
+In array size was fixed and similar type of data had to be saved[int, string]. We can overcome the disadvantage of array using ArrayList.
+ArrayList is dynamic array[ArrayList internally uses an array - it can grow and shrink automatically]
+ArrayList uses index to store elements
+ArrayList allows duplicate elements
+ArrayList maintains insertion order
+
+add()	 						- is used to add elements
+add(index, element) 			- is used to add data at a particular index
+addAll(Collection c) 			- is used to add the data of one array list to another array list 
+addAll(index, Collection c)		- is used to add another list at a particular index
+removeAll(Collection c) 		- is used to remove matching data present in list2 from list1
+remove(index)					- is used to remove data based on index
+retainAll(Collection c)         - is used to retain the data that matches with list2 in list1
+size()							- gives size of array list / no:of elements present in array list
+get(index)						- returns data present at a particular index
+contains(element)				- returns boolean value based on if the element is present/not in array list
+containsAll(Collection c)		- if all the data is present in list1, then it returns true
+
+	 */
+
+}
